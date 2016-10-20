7fac9cc2499142f8e44dd8e85721618f1be75740
dcy <766320183@qq.com>
2016-10-20 14:32:13 +0800

    集成eyunpian进来

---
+++ examples/yunpian.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import pika
+import json
+
+HOST = 'localhost'
+USERNAME = 'hisir'
+PASSWORD = 'hisir123'
+
+class Yunpian():
+    def __init__(self):
+        credentials = pika.PlainCredentials(USERNAME, PASSWORD)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, credentials=credentials))
+        self.channel = self.connection.channel()
+        self.queue = 'yunpian'
+
+    def single_send(self):
+        data = {'type': 'single',
+                'mobile': '15102025006',
+		'content':'【广州灵光科技】验证码6666，请您尽快验证，完成Hisir注册。如非本人操作请忽略。'}
+        self.in_mq(data)
+
+    def batch_send(self):
+        data = {'type': 'batch',
+                'mobile': '15102025006',
+		'content':'【广州灵光科技】验证码6666，请您尽快验证，完成Hisir注册。如非本人操作请忽略。'}
+        self.in_mq(data)
+
+
+
+    def end(self):
+        self.channel.close()
+        self.connection.close()
+
+    def in_mq(self, data):
+        self.channel.basic_publish(exchange='',
+                routing_key=self.queue,
+                body=json.dumps(data))
+
+
+if __name__ == "__main__":
+    yunpian = Yunpian()
+
+    #yunpian.single_send()
+    yunpian.batch_send()
+
+    yunpian.end()
--- src/epush_sms.erl
+++ src/epush_sms.erl
@@ -1,7 +1,6 @@
 -module(epush_sms).
 -export([handle_send/2,
-         loop/4, handle_info/2,
-         single_test/0, batch_test/0
+         loop/4, handle_info/2
         ]).
 -include("epush.hrl").
 -include_lib("amqp_client/include/amqp_client.hrl").
@@ -14,43 +13,9 @@ handle_send(yunpian, MQPayload, #{apikey:=Apikey}) ->
     yunpian_send(Type, Apikey, Mobile, Content).
 
 yunpian_send(<<"single">>, Apikey, Mobile, Content) ->
-    Datas = #{apikey => Apikey, mobile => Mobile, text => Content},
-    Payload = epush_util:urlencode(Datas),
-    URL = <<"http://sms.yunpian.com/v2/sms/single_send.json">>,
-    Method = post,
-    Options = [{pool, sms}],
-    Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded; charset=utf-8">>}],
-    {ok, _StatusCode, _RespHeaders, ClientRef} = hackney:request(Method, URL, Headers,
-                                                                 Payload, Options),
-    {ok, ResultBin} = hackney:body(ClientRef),
-    Result = jiffy:decode(ResultBin, [return_maps]),
-    case maps:get(<<"code">>, Result) of
-        0 ->
-            ok;
-        _ ->
-            ?ERROR_MSG("epush sms yunpian single_send error, Mobile: ~p, Result: ~p", [Mobile, Result]),
-            error
-    end;
+    eyunpian_sms:single_send(Apikey, Mobile, Content);
 yunpian_send(<<"batch">>, Apikey, Mobile, Content) ->
-    Datas = #{apikey => Apikey, mobile => Mobile, text => Content},
-    Payload = epush_util:urlencode(Datas),
-    URL = <<"http://sms.yunpian.com/v2/sms/batch_send.json">>,
-    Method = post,
-    Options = [{pool, sms}],
-    Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded; charset=utf-8">>}],
-    {ok, _StatusCode, _RespHeaders, ClientRef} = hackney:request(Method, URL, Headers,
-                                                                 Payload, Options),
-    {ok, ResultBin} = hackney:body(ClientRef),
-    Result = jiffy:decode(ResultBin, [return_maps]),
-    #{<<"total_count">> := TotalCount} = Result,
-    case TotalCount of
-        0 ->
-            ?ERROR_MSG("epush sms yunpian batch_send no success, Mobile: ~p", [Mobile]),
-            ok;
-        _ ->
-            ok
-    end.
-
+    eyunpian_sms:batch_send(Apikey, Mobile, Content).
 
 
 
@@ -59,48 +24,9 @@ loop(_RoutingKey, _ContentType, Payload, State) ->
     %% yunpian 那边返回码很多情况
     case handle_send(Payload, State) of
         ok -> {ack, State};
-        error -> {ack, State}
+        error -> {ack, State};
+        {error, _} -> {ack, State}
     end.
 
 handle_info(_Info, State) ->
     {ok, State}.
-
-
-
-
-
-
-
-single_test() ->
-    {ok, Connection} =
-    amqp_connection:start(#amqp_params_network{host = "localhost"}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-
-    Mobile = "15102025006",
-    Content = unicode:characters_to_binary("【广州灵光科技】验证码6666，请您尽快验证，完成Hisir注册。如非本人操作请忽略。"),
-
-    amqp_channel:cast(Channel,
-                      #'basic.publish'{exchange = <<"">>,
-                                       routing_key = <<"yunpian">>},
-                      #amqp_msg{payload = jiffy:encode(#{mobile=>Mobile, content=>Content, type=>"single"})}),
-    amqp_channel:close(Channel),
-    amqp_connection:close(Connection),
-    ok.
-
-batch_test() ->
-    {ok, Connection} =
-    amqp_connection:start(#amqp_params_network{host = "localhost"}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-
-	Mobile = "15102025006,15013276061",
-    Content = unicode:characters_to_binary("【广州灵光科技】验证码8666，请您尽快验证，完成Hisir注册。如非本人操作请忽略。"),
-
-    amqp_channel:cast(Channel,
-                      #'basic.publish'{exchange = <<"">>,
-                                       routing_key = <<"yunpian">>},
-                      #amqp_msg{payload = jiffy:encode(#{mobile=>Mobile, content=>Content, type=>"batch"})}),
-    amqp_channel:close(Channel),
-    amqp_connection:close(Connection),
-    ok.
-
-
